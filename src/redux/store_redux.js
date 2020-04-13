d14ad57ae3aa28d5dfb0db25bdfae5254857d91a
Margo <margokomilfo@mail.ru>
2020-04-13 15:11:25 +0200

    91 - redux-devtools-extention-compose in redux_store

--- src/redux/store_redux.js
+++ src/redux/store_redux.js
@@ -21,4 +21,5 @@ let reducers = combineReducers(
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers,  composeEnhancers(applyMiddleware(thunk)));
 
+
 export default store; 
