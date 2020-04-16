cefefd919863454de6573ce3517cacd9669d1e3b
Margo <margokomilfo@mail.ru>
2020-04-16 22:51:53 +0200

    93-React.Suspense & React.lazy

--- src/TriM.js
+++ src/TriM.js
@@ -1,27 +1,31 @@
-/* eslint-disable react/jsx-pascal-case */
 import React from 'react'
 import './TriM.js'
 import './TriM.css'
 import Sitebar from './components/Sitebar/Sitebar.jsx'
 import {Route, withRouter} from 'react-router-dom'
 import Last_seen from './components/Last_seen/Last_seen.jsx'
-import Message from './components/Sitebar/Message/Message.jsx'
 import Photos from './components/Sitebar/Photos/Photos.jsx'
 import Friends from './components/Sitebar/Friends/Friends.jsx'
 import News from './components/Sitebar/News/News.jsx'
 import Music from './components/Sitebar/Music/Music.jsx'
 import Muvies from './components/Sitebar/Muvies/Muvies.jsx'
 import Groups from './components/Sitebar/Groups/Groups.jsx'
-import AllUsersContainer from "./components/Sitebar/AllUsers/AllUsersContainer";
+
 import MainContainer from "./components/Main/MainContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Header/Login/Login";
+
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializationTriM} from "./redux/trim_reducer";
 import Loader from "./components/common/Loader/Loader";
 import {getInitialed, getIsLogin} from "./redux/selectors";
 
+const Message = React.lazy(() => import('./components/Sitebar/Message/Message.jsx'));
+// import Message from './components/Sitebar/Message/Message.jsx'
+const AllUsersContainer = React.lazy(() => import('./components/Sitebar/AllUsers/AllUsersContainer'));
+//import AllUsersContainer from "./components/Sitebar/AllUsers/AllUsersContainer";
+const Login = React.lazy(() => import('./components/Header/Login/Login'));
+//import Login from "./components/Header/Login/Login";
 
 class TriM extends React.Component {
     componentDidMount() {
@@ -38,14 +42,25 @@ class TriM extends React.Component {
                     <div className='wrapper'>
                         <Sitebar/>
                         <div className='main_wrapper'>
-                            {this.props.isLogin ? <Last_seen last_seen='Online...'/> :
-                                <Last_seen last_seen={`You aren't autorized...`}/>}
+                            {this.props.isLogin ? (<Last_seen last_seen='Online...'/>) :
+                                (<Last_seen last_seen={`You aren't autorized...`}/>) }
                             <Route path='/profile/:userId?/' render={() => <MainContainer store={this.props.store}
                                                                                           state={this.props.state}/>}/>
-                            <Route path='/message' render={() => <Message store={this.props.store}
-                                                                          userData={this.props.state.messagePage.userData}/>}/>
-                            <Route path='/users' render={() => <AllUsersContainer/>}/>
-                            <Route path='/login' render={() => <Login/>}/>
+                            <Route path='/message' render={() =>
+                                <React.Suspense fallback={<Loader/>}>
+                                    <Message store={this.props.store} userData={this.props.state.messagePage.userData}/>
+                                </React.Suspense>}/>
+
+                            <Route path='/users' render={() =>
+                                <React.Suspense fallback={<Loader/>}>
+                                    <AllUsersContainer/>
+                                </React.Suspense>}/>
+
+
+                            <Route path='/login' render={() =>
+                                <React.Suspense fallback={<Loader/>}>
+                                    <Login/>
+                                </React.Suspense>}/>
 
                             <Route path='/photos' render={() => <Photos/>}/>
                             <Route path='/friends' render={() => <Friends/>}/>
