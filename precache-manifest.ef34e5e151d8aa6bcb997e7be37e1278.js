dac61cfe9fe367f6b331c2045439c1e4892c2847
Margo <margokomilfo@mail.ru>
2020-04-17 22:37:18 +0200

    Updates

---
+++ precache-manifest.ef34e5e151d8aa6bcb997e7be37e1278.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8882211dad86e40142ddcd15dbf7f28c",
+    "url": "/TriMsocial-REACT/index.html"
+  },
+  {
+    "revision": "1fb68ec882542fd23fad",
+    "url": "/TriMsocial-REACT/static/css/3.22960b66.chunk.css"
+  },
+  {
+    "revision": "163b7e0573dbf50eb967",
+    "url": "/TriMsocial-REACT/static/css/4.5bc508a5.chunk.css"
+  },
+  {
+    "revision": "9cffd5b9d764ff8b0279",
+    "url": "/TriMsocial-REACT/static/css/5.703f550d.chunk.css"
+  },
+  {
+    "revision": "45be0b28624fb44cd1a7",
+    "url": "/TriMsocial-REACT/static/css/main.21a619e3.chunk.css"
+  },
+  {
+    "revision": "b84961bc1238d328f438",
+    "url": "/TriMsocial-REACT/static/js/2.f0782aed.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/TriMsocial-REACT/static/js/2.f0782aed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb68ec882542fd23fad",
+    "url": "/TriMsocial-REACT/static/js/3.078fd3bc.chunk.js"
+  },
+  {
+    "revision": "163b7e0573dbf50eb967",
+    "url": "/TriMsocial-REACT/static/js/4.d66b2928.chunk.js"
+  },
+  {
+    "revision": "9cffd5b9d764ff8b0279",
+    "url": "/TriMsocial-REACT/static/js/5.3fee8d0b.chunk.js"
+  },
+  {
+    "revision": "45be0b28624fb44cd1a7",
+    "url": "/TriMsocial-REACT/static/js/main.1873be83.chunk.js"
+  },
+  {
+    "revision": "50ead9069fe7f409a9fb",
+    "url": "/TriMsocial-REACT/static/js/runtime-main.6256ed53.js"
+  },
+  {
+    "revision": "1d5193615710cf4181fc8014143a7701",
+    "url": "/TriMsocial-REACT/static/media/loader.1d519361.gif"
+  },
+  {
+    "revision": "fe6b73b0e08e2c91ef3c0b7d0aefa3c1",
+    "url": "/TriMsocial-REACT/static/media/logo.fe6b73b0.png"
+  },
+  {
+    "revision": "63de91b9e50aeecce7e609a608b81767",
+    "url": "/TriMsocial-REACT/static/media/message.63de91b9.png"
+  },
+  {
+    "revision": "57c40c67e1838f2586727bc01713a567",
+    "url": "/TriMsocial-REACT/static/media/nophoto.57c40c67.png"
+  }
+]);
